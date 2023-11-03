b954d8fa40ed95c89e3964b5c4666ede240df377
ibnufajar0104 <ibnufajar0104@gmail.com>
2023-11-03 11:15:10 +0800

    .

--- application/views/registrasi_form/js.php
+++ application/views/registrasi_form/js.php
@@ -1,54 +1,86 @@
 <script>
-var global_url = '<?= base_url('registrasi/') ?>';
-$('document').ready(function() {
+    var global_url = '<?= base_url('registrasi/') ?>';
+    $('document').ready(function() {
 
-    $('#jenis').val('');
-    $('.npwp').hide();
-    $('.nik').hide();
+        $('#jenis').val('');
+        $('.npwp').hide();
+        $('.nik').hide();
 
-    $('#jenis').change(function() {
+        $('#jenis').change(function() {
 
-        if ($(this).val() == 1) {
-            $('.npwp').show();
-            $('.nik').hide();
-            $("#tblpemohon_npwp").prop('required', true);
-            $("#tblpemohon_noidentitas").prop('required', false);
-        } else {
-            $('.npwp').hide();
-            $('.nik').show();
-
-            $("#tblpemohon_npwp").prop('required', false);
-            $("#tblpemohon_noidentitas").prop('required', true);
-        }
-    });
-});
-
-
-$('.form-daftar').submit(function(e) {
-
-    event.preventDefault(); // Menghentikan perilaku bawaan formulir (misalnya, mengirimkan permintaan GET)
-
-    var url = global_url + 'daftar'
-    var formData = $(this).serialize();
-    postWithAjax(url, formData, function(response, error) {
-        if (error) {
-            console.error(error);
-        } else {
-
-
-            if (response.status) {
-
-                success(response.msg);
-                new_location('<?= site_url('login') ?>');
+            if ($(this).val() == 1) {
+                $('.npwp').show();
+                $('.nik').hide();
+                $("#tblpemohon_npwp").prop('required', true);
+                $("#tblpemohon_noidentitas").prop('required', false);
             } else {
-                fail(response.msg);
+                $('.npwp').hide();
+                $('.nik').show();
+
+                $("#tblpemohon_npwp").prop('required', false);
+                $("#tblpemohon_noidentitas").prop('required', true);
+            }
+        });
+    });
+
+
+    $('.form-daftar').submit(function(e) {
+
+        event.preventDefault(); // Menghentikan perilaku bawaan formulir (misalnya, mengirimkan permintaan GET)
+
+        var password = $('#password');
+        var konfirmasi = $('#password');
+
+
+        if (!validatePassword(password)) {
+            alert(
+                'Password paling sedikit 6 karakter yang mengandung 1 angka, 1 huruf kapital dan 1 karakter')
+            return false;
+        }
+
+        var url = global_url + 'daftar'
+        var formData = $(this).serialize();
+        postWithAjax(url, formData, function(response, error) {
+            if (error) {
+                console.error(error);
+            } else {
+
+
+                if (response.status) {
+
+                    success(response.msg);
+                    new_location('<?= site_url('login') ?>');
+                } else {
+                    fail(response.msg);
+                }
+
+
             }
 
+            after_load('Mendaftar');
+        });
 
-        }
-
-        after_load('Mendaftar');
     });
 
-});
+    function validatePassword(password) {
+        // Define the regular expressions for each password rule
+        const minLengthRegex = /.{6,}/; // Minimum 6 characters
+        const uppercaseRegex = /[A-Z]/; // At least one uppercase letter
+        const digitRegex = /[0-9]/; // At least one digit
+        const specialCharRegex = /[$@$!%*?&]/; // At least one special character
+
+        // Check if the password satisfies all the rules
+        const isMinLengthValid = minLengthRegex.test(password);
+        const isUppercaseValid = uppercaseRegex.test(password);
+        const isDigitValid = digitRegex.test(password);
+        const isSpecialCharValid = specialCharRegex.test(password);
+
+        // Return true if all rules are satisfied, false otherwise
+        return (
+            isMinLengthValid &&
+            isUppercaseValid &&
+            isDigitValid &&
+            isSpecialCharValid
+        );
+    }
 </script>
--- application/views/registrasi_form/view.php
+++ application/views/registrasi_form/view.php
@@ -31,16 +31,16 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group mt-4">
-                                            <label for="jenis_pemohon">Nama Pemohon</label>
+                                            <label for="jenis_pemohon">Nama</label>
                                             <input type="text" name="tblpemohon_nama" id="tblpemohon_nama"
-                                                class="form-control">
+                                                class="form-control" required>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group mt-4">
-                                            <label for="jenis_pemohon">Nomor WA Pemohon</label>
+                                            <label for="jenis_pemohon">Nomor WA Aktif</label>
                                             <input type="text" name="tblpemohon_telpon" id="tblpemohon_telpon"
-                                                class="form-control">
+                                                class="form-control" required>
                                         </div>
                                     </div>
 
@@ -49,16 +49,16 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group mt-4">
-                                            <label for="jenis_pemohon">NPWP Pemohon</label>
+                                            <label for="jenis_pemohon">NPWP </label>
                                             <input type="text" name="tblpemohon_npwp" id="tblpemohon_npwp"
-                                                class="form-control">
+                                                class="form-control" required>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group mt-4">
-                                            <label for="jenis_pemohon"> Email Pemohon</label>
+                                            <label for="jenis_pemohon"> Email Aktif</label>
                                             <input type="text" name="tblpemohon_email" id="tblpemohon_email"
-                                                class="form-control">
+                                                class="form-control" required>
                                         </div>
                                     </div>
 
@@ -71,7 +71,7 @@
                                             <label for="jenis_pemohon">Alamat</label>
                                             <textarea name="tblpemohon_alamat" id="tblpemohon_alamat"
                                                 class="form-control" placeholder="Alamat/RT/RW/Kecamatan/Kelurahan"
-                                                rows="4"></textarea>
+                                                rows="4" required></textarea>
 
                                         </div>
                                     </div>
@@ -84,7 +84,7 @@
                                         <div class="form-group mt-4">
                                             <label for="jenis_pemohon">Username</label>
                                             <input type="text" name="username" id="username" class="form-control"
-                                                value="<?= $this->session->username ?>" readonly>
+                                                value="<?= $this->session->username ?>" readonly required>
                                         </div>
                                     </div>
 
@@ -102,7 +102,7 @@
                                         <div class="form-group mt-4">
                                             <label for="jenis_pemohon">Password</label>
                                             <input type="password" name="tblpengguna_password" id="tblpengguna_password"
-                                                class="form-control">
+                                                class="form-control" required>
                                         </div>
                                     </div>
 
@@ -110,7 +110,7 @@
                                         <div class="form-group mt-4">
                                             <label for="jenis_pemohon">Konfirmasi Password</label>
                                             <input type="password" name="konfirmasi" id="konfirmasi"
-                                                class="form-control">
+                                                class="form-control" required>
                                         </div>
                                     </div>
                                 </div>
